95059aedc310c5e3898306842aee29fd3ad59a55
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2024-03-19 18:21:18 +0500

    Create Prisma Schema

---
+++ src/Components/Todos.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Todo } from "@prisma/client";
+
+const Todos = ({ todo }: { todo: Todo }) => {
+    return (
+        <div>
+        </div>
+    )
+};
+
+export default Todos;
